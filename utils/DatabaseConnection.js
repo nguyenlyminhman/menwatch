8fc73915a093162daa161de6d59afc3404ac6069
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-03-20 12:38:53 +0700

    finished. edited DB connection config. Code is running well

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -27,18 +27,18 @@ var config = {
 //const URI = 'postgres://ynixgrygsibkhx:dc4fc74d9e74195ead59737bff32dfb5351b2c558bef07ba167d96f5b82da7ef@ec2-23-21-220-48.compute-1.amazonaws.com:5432/d15n4528dno5aj'
 //const URI = 'postgres://postgres:sa@localhost:5432/menstyle'
 
-// function queryDB(sqlString, arrData) {
-//     return new Promise((resolve, reject) => {
-//         pool.connect((err, client, done) => {
-//             if (err) return reject(err);
-//             done();
-//             client.query(sqlString, arrData, (errQuery, result) => {
-//                 if (errQuery) return reject(errQuery);
-//                 resolve(result);
-//             });
-//         });
-//     });
-// }
+function queryDB(sqlString, arrData) {
+    return new Promise((resolve, reject) => {
+        pool.connect((err, client, done) => {
+            if (err) return reject(err);
+            done();
+            client.query(sqlString, arrData, (errQuery, result) => {
+                if (errQuery) return reject(errQuery);
+                resolve(result);
+            });
+        });
+    });
+}
 
 // queryDB('select * from "Customer"', [])
 // .then(data => console.log(data.rows))
