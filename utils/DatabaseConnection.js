734c72d8627a226efcda744239c2764ecd5c34dd
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-03-20 12:23:37 +0700

    edited config file

--- utils/DatabaseConnection.js
+++ utils/DatabaseConnection.js
@@ -1,14 +1,14 @@
 const pg = require('pg');
 //using for local host
-var config = {
-    user: 'postgres', //env var: PGUSER
-    database: 'menwatch', //env var: PGDATABASE
-    password: 'sa', //env var: PGPASSWORD
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, //env var: PGPORT
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
-};
+// var config = {
+//     user: 'postgres', //env var: PGUSER
+//     database: 'menwatch', //env var: PGDATABASE
+//     password: 'sa', //env var: PGPASSWORD
+//     host: 'localhost', // Server hosting the postgres database
+//     port: 5432, //env var: PGPORT
+//     max: 10, // max number of clients in the pool
+//     idleTimeoutMillis: 1000, // how long a client is allowed to remain idle before being closed
+// };
 
 // using for heroku
 var config = {
