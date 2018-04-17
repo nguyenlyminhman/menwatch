d16f0325c8cc771ed5760c16a0c9f60bf1e955bb
nguyenlyminhman <nguyenlyminhman@gmail.com>
2018-04-17 09:06:22 +0700

    Finish. Edit product information. Code is running well

--- controller/admin/postAd_ProductUpdate.js
+++ controller/admin/postAd_ProductUpdate.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
 let Product = require('../../model/Product');
 let { removeSpace } = require('../../utils/Tools');
 let { getManyFieldnameUpload } = require('../../utils/ImageUpload');
+
+
 
 
 let upload = getManyFieldnameUpload('image1', 'image2', 'image3');
@@ -9,17 +12,15 @@ module.exports = (req, res, next) => {
 
     upload(req, res, err => {
         if (err) return res.send('Error upload product img: ' + err);
-
         let { id } = req.params;
         let { idBrand, idStyle, name, price, quantity, description, cs, mt, wr, sm, status, hdimg01, hdimg02, hdimg03 } = req.body;
-        
         let img1 = req.files['image1'] ? req.files['image1'][0].filename : hdimg01;
         let img2 = req.files['image2'] ? req.files['image2'][0].filename : hdimg02;
         let img3 = req.files['image3'] ? req.files['image3'][0].filename : hdimg03;
         let image = '{"img3":"' + img3 + '","img2":"' + img2 + '","img1":"' + img1 + '"}';
 
         let details = '{"cs":"' + removeSpace(cs) + '","mt":"' + removeSpace(mt) + '","wr":"' + removeSpace(wr) + '","sm":"' + removeSpace(sm) + '"}';
-        
+
         let product = new Product(id, idStyle, idBrand, removeSpace(name), price, quantity, removeSpace(description), image, details, status);
         product.updateProductInfo().
             then(result => {
